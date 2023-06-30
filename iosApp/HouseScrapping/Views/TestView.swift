ce76b07cbfe0c8404138820263b7d9dc6be579df
jingchengpeng@yahoo.com <jingchengpeng@yahoo.com>
2023-06-30 00:30:36 -0400

    image selection working

--- iosApp/HouseScrapping/Views/TestView.swift
+++ iosApp/HouseScrapping/Views/TestView.swift
@@ -1,60 +1,39 @@
 //
-//  testView.swift
+//  TestView.swift
 //  HouseScrapping
 //
-//  Created by Justin Peng on 2023-06-27.
+//  Created by Justin Peng on 2023-06-29.
 //
 
-import PhotosUI
 import SwiftUI
-import FirebaseFirestore
-import FirebaseAuth
-let databaseReference = Firestore.firestore().collection("users")
 
 struct TestView: View {
-    @State var selectedItems: [PhotosPickerItem] = []
-    @State var data: Data?
-    
+    @State var isPickerShowing = false
+    @State var selectedImage: UIImage?
     var body: some View {
-        VStack{
-            if let data = self.data, let uiimage = UIImage(data: data){
-                Image(uiImage: uiimage)
+        
+        VStack {
+            if selectedImage != nil{
+                Image(uiImage: selectedImage!)
                     .resizable()
+                    .frame(width: 200, height: 200)
+            }
+            Button{
+                isPickerShowing = true
+            } label: {
+                Text("Select a photo")  
             }
             
-            Spacer()
-            PhotosPicker(
-                selection: $selectedItems,
-                maxSelectionCount: 1,
-                matching: .images
-            ){
-                Text("Pick photos")
-            }
-            .onChange(of: selectedItems){ newValue in
-                guard let item = selectedItems.first else {
-                    return
-                }
-            
-                item.loadTransferable(type: Data.self){ result in
-                    switch result {
-                    case .success(let data):
-                        if let data = data {
-                            self.data = data
-                            
-                        } else {
-                            print("Data is nil")
-                        }
-                    
-                    case .failure(let failure):
-                        fatalError("\(failure)")
-                    }
-                }
-            }
         }
+        .sheet(isPresented: $isPickerShowing, onDismiss: nil){
+            // Image picker
+            ImagePicker(selectedImage: $selectedImage, isPickerShowing: $isPickerShowing)
+        }
+        
     }
 }
 
-struct testView_Previews: PreviewProvider {
+struct TestView_Previews: PreviewProvider {
     static var previews: some View {
         TestView()
     }
