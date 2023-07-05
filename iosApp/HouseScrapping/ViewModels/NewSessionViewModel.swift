05d97d423114a8ba2ac0c9b3fa54e417cafa2e49
jingchengpeng@yahoo.com <jingchengpeng@yahoo.com>
2023-07-04 20:46:37 -0400

    Create session and push to DB

---
+++ iosApp/HouseScrapping/ViewModels/NewSessionViewModel.swift
@@ -0,0 +1,46 @@
+//
+//  TestViewModel.swift
+//  HouseScrapping
+//
+//  Created by Justin Peng on 2023-07-03.
+//
+
+
+
+import Foundation
+import FirebaseAuth
+import FirebaseFirestore
+
+class NewSessionViewModel: ObservableObject {
+    
+   
+    init(){}
+    
+    func save(course: String, location: String){
+        
+        // Create model
+        let newId = UUID().uuidString
+        let newSession = Session(
+            id: newId,
+            course: course,
+            location: location,
+            date: Date(timeIntervalSince1970: Date().timeIntervalSince1970)
+        )
+        
+        // Save model
+        let db = Firestore.firestore()
+        db.collection("sessions")
+            .document(newId)
+            .setData(newSession.asDictionary()) { error in
+                if let error = error {
+                    print("Error saving session: \(error)")
+                } else {
+                    print("Session saved successfully!")
+                }
+            }
+    
+    }
+    /// Delete to do list item
+    /// - Parameter id: item id to delete
+    
+}
