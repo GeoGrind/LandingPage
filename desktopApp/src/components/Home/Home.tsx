38f4d137c2bdccffef9da32594310807a9af8647
Delbert <delbert3luo@gmail.com>
2023-07-14 23:47:21 -0400

    added custom markers and scroll zoom

--- desktopApp/src/components/Home/Home.tsx
+++ desktopApp/src/components/Home/Home.tsx
@@ -1,24 +1,37 @@
-import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import { MapContainer, TileLayer, Marker } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
+import { Icon } from 'leaflet';
 import styles from './Home.module.scss';
+import markerIconPng from '../../../assets/956fd6.png';
 
+// TODO: fetch the data from firebase
 function Map() {
   return (
     <MapContainer
       className={styles.Map}
       center={[43.472286, -80.544861]}
+      zoomDelta={0.1}
       zoom={14}
-      scrollWheelZoom={false}
+      minZoom={0}
+      maxZoom={22}
+      scrollWheelZoom
     >
       <TileLayer
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         attribution="Map data © OpenStreetMap contributors"
       />
 
-      <Marker position={[43.472286, -80.544861]}>
-        <Popup>
-          A pretty CSS3 popup. <br /> Easily customizable.
-        </Popup>
+      <Marker
+        position={[43.472286, -80.544861]}
+        icon={
+          new Icon({
+            iconUrl: markerIconPng,
+            iconSize: [25, 41],
+            iconAnchor: [12, 41],
+          })
+        }
+      >
+        test
       </Marker>
     </MapContainer>
   );
