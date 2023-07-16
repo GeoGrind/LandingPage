beb863352bd07f0e9b14fc46fd5483bd24cba6dc
Delbert <delbert3luo@gmail.com>
2023-07-15 21:26:35 -0400

    remove misleading comments for null location

--- desktopApp/src/types/location.type.ts
+++ desktopApp/src/types/location.type.ts
@@ -1,5 +1,3 @@
-// Null location is defined as (0,0)
-
 export type Location = {
   longitude: number;
   latitude: number;
--- desktopApp/src/types/user.type.ts
+++ desktopApp/src/types/user.type.ts
@@ -4,7 +4,7 @@ import { Session } from './session.type';
 export type User = {
   uid: string;
   email: string;
-  location: Location | null; // 0,0 means null location
+  location: Location | null;
   isInSession: boolean;
   onGoingSession: Session | null;
 };
