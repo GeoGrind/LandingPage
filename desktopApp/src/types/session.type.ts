e8fc668bcea34e5a763e1e6ac3a5b7e55985b3b5
Delbert <delbert3luo@gmail.com>
2023-07-19 23:14:23 -0400

    create session, add user to db on signup, update types

--- desktopApp/src/types/session.type.ts
+++ desktopApp/src/types/session.type.ts
@@ -2,4 +2,7 @@ export type Session = {
   course: string;
   startTime: number; // time since 1970 in milliseconds
   isVisible: boolean;
+  sessionStartLocation: Location | null;
+  numberOfCheerers: number;
+  cheerers: string[];
 };
--- desktopApp/src/utils/db.ts
+++ desktopApp/src/utils/db.ts
@@ -1,6 +1,14 @@
-import { collection, getDocs } from 'firebase/firestore';
+import {
+  collection,
+  doc,
+  getDocs,
+  setDoc,
+  updateDoc,
+} from 'firebase/firestore';
 import { User } from 'types/user.type';
-import { FIREBASE_DB } from '../firebase';
+import { Session } from 'types/session.type';
+import { Location } from 'types/location.type';
+import { FIREBASE_AUTH, FIREBASE_DB } from '../firebase';
 
 export const fetchActiveUsers = async (): Promise<User[]> => {
   const docRef = collection(FIREBASE_DB, 'users');
@@ -14,4 +22,33 @@ export const fetchActiveUsers = async (): Promise<User[]> => {
   return users;
 };
 
+export const createSession = async (course: string, location: Location) => {
+  try {
+    if (!FIREBASE_AUTH.currentUser) {
+      return;
+    }
+    const session: Session = {
+      course,
+      startTime: Date.now(),
+      isVisible: true,
+      sessionStartLocation: location,
+      numberOfCheerers: 0,
+      cheerers: [],
+    };
+    const userRef = doc(FIREBASE_DB, 'users', FIREBASE_AUTH.currentUser.uid);
+    await updateDoc(userRef, { onGoingSession: session });
+    await updateDoc(userRef, { isInSession: true });
+    await updateDoc(userRef, { location });
+  } catch (error) {
+    console.log("Erorr updating user's session:", error);
+  }
+};
+
+export const createUser = async (user: User) => {
+  try {
+    await setDoc(doc(FIREBASE_DB, 'users', user.uid), user);
+  } catch (error) {
+    console.log('Error creating user:', error);
+  }
+};
 export const dontUse = 2; // prevents eslint error exporting one thing, will export more util later
