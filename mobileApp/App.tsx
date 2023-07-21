e34643afcdc0f80688edb9367a7bd754421ce7a2
Justin-Jingcheng-Peng <jingchengpeng@yahoo.com>
2023-07-21 12:04:31 -0400

    email verification working

--- mobileApp/App.tsx
+++ mobileApp/App.tsx
@@ -1,37 +1,37 @@
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import Login from './app/screens/Login';
-import Map from './app/screens/Map';
-import Profile from './app/screens/Profile';
-import { useEffect, useState } from 'react';
-import { User, onAuthStateChanged } from 'firebase/auth';
-import { FIREBASE_AUTH } from './FirebaseConfig';
-import { getUserLocationAndStoreInDb } from './app/utils/db';
-import { Provider } from 'react-redux';
-import {store} from './app/store/store';
-import Test from './app/screens/Test'
-import { Keyboard } from 'react-native';
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import Login from "./app/screens/Login";
+import Map from "./app/screens/Map";
+import Profile from "./app/screens/Profile";
+import { useEffect, useState } from "react";
+import { User, onAuthStateChanged } from "firebase/auth";
+import { FIREBASE_AUTH } from "./FirebaseConfig";
+import { getUserLocationAndStoreInDb } from "./app/utils/db";
+import { Provider } from "react-redux";
+import { store } from "./app/store/store";
+import Test from "./app/screens/Test";
+import { Keyboard } from "react-native";
 
 const Stack = createNativeStackNavigator();
 
 const InsideStack = createNativeStackNavigator();
 
-function InsideLayout(){
+function InsideLayout() {
   return (
     <InsideStack.Navigator>
       <InsideStack.Screen name="Map" component={Map} />
       <InsideStack.Screen name="Profile" component={Profile} />
       <InsideStack.Screen name="Test" component={Test} />
     </InsideStack.Navigator>
-  )
+  );
 }
 
-export default function App() { 
+export default function App() {
   const [user, setUser] = useState<User | null>(null);
-  
+
   const printStuff = () => {
-    console.log("stuff")
-  }
+    console.log("stuff");
+  };
 
   const handleKeyboardFrameChange = (event: any) => {
     onAuthStateChanged(FIREBASE_AUTH, (user) => {
@@ -40,25 +40,32 @@ export default function App() {
   };
 
   useEffect(() => {
-    const keyboardListener = Keyboard.addListener('keyboardWillChangeFrame', handleKeyboardFrameChange);
+    const keyboardListener = Keyboard.addListener(
+      "keyboardWillChangeFrame",
+      handleKeyboardFrameChange
+    );
     return () => keyboardListener.remove();
   }, []);
 
-  return ( 
+  return (
     <Provider store={store}>
       <NavigationContainer>
         <Stack.Navigator initialRouteName="Login">
-        {user && user.emailVerified ? (
-            <Stack.Screen name="Inside" component={InsideLayout} options={{headerShown:false}} />
+          {user && user.emailVerified ? (
+            <Stack.Screen
+              name="Inside"
+              component={InsideLayout}
+              options={{ headerShown: false }}
+            />
           ) : (
-            <Stack.Screen name="Login" component={Login} options={{headerShown:false}}/>
-          ) }
+            <Stack.Screen
+              name="Login"
+              component={Login}
+              options={{ headerShown: false }}
+            />
+          )}
         </Stack.Navigator>
       </NavigationContainer>
     </Provider>
-    
   );
 }
-
-
-
