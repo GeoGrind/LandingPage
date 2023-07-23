f78f70e63a25fb546e3e5c7b87abf3432a1cbbc9
Delbert <delbert3luo@gmail.com>
2023-07-22 23:55:25 -0400

    add skeleton account info

--- desktopApp/src/components/Authentication/Account/Account.tsx
+++ desktopApp/src/components/Authentication/Account/Account.tsx
@@ -1,12 +1,15 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { NavLink, useNavigate } from 'react-router-dom';
+import { User } from 'types/user.type';
+import { Location } from 'types/location.type';
+import { getCurrentUser } from 'utils/db';
+import { signOut } from 'firebase/auth';
 import { FIREBASE_AUTH } from '../../../firebase';
 import styles from './Account.module.scss';
-import FormItem from '../Form/FormItem/FormItem';
-import { signOut } from 'firebase/auth';
 
 function Account() {
   const navigate = useNavigate();
+  const [curUser, setCurUser] = useState<User | undefined>(undefined);
 
   const handleLogout = () => {
     signOut(FIREBASE_AUTH)
@@ -21,6 +24,9 @@ function Account() {
         // An error happened.
       });
   };
+  useEffect(() => {
+    getCurrentUser(setCurUser);
+  }, []);
 
   return (
     <div className={styles.Account}>
@@ -31,6 +37,36 @@ function Account() {
       >
         Logout
       </button>
+
+      {curUser && (
+        <div>
+          profilePicture: {curUser.profilePicture}
+          <br />
+          User Properties: uid: {curUser.uid}
+          <br />
+          email: {curUser.email}
+          <br />
+          isInSession: {curUser.isInSession}
+          <br />
+          Session: {curUser.onGoingSession?.course}
+          <br />
+          course: {curUser.onGoingSession?.course}
+          <br />
+          startTime: {curUser.onGoingSession?.startTime}
+          <br />
+          isVisible: {curUser.onGoingSession?.isVisible}
+          <br />
+          sessionStartLocationLong:{' '}
+          {curUser.onGoingSession?.sessionStartLocation?.longitude}
+          <br />
+          sessionStartLocationLat:{' '}
+          {curUser.onGoingSession?.sessionStartLocation?.latitude}
+          <br />
+          numberOfCheerers: {curUser.onGoingSession?.numberOfCheerers}
+          <br />
+          cheerers: to add
+        </div>
+      )}
     </div>
   );
 }
--- desktopApp/src/types/session.type.ts
+++ desktopApp/src/types/session.type.ts
@@ -1,3 +1,5 @@
+import { Location } from 'types/location.type';
+
 export type Session = {
   course: string;
   startTime: number; // time since 1970 in milliseconds
