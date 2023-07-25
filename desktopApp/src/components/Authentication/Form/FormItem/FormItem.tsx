9320629c20deddb76f3c9eca17eecb5d85f2b7d2
Delbert <delbert3luo@gmail.com>
2023-07-24 22:42:01 -0400

    inc change to account

--- desktopApp/src/components/Authentication/Form/FormItem/FormItem.tsx
+++ desktopApp/src/components/Authentication/Form/FormItem/FormItem.tsx
@@ -3,10 +3,11 @@ import styles from './FormItem.module.scss';
 
 interface IFormItemProps {
   label: string;
+  placeholder?: string;
   onChange: (value: React.SetStateAction<string>) => void;
 }
 
-function FormItem({ label, onChange }: IFormItemProps) {
+function FormItem({ label, placeholder, onChange }: IFormItemProps) {
   return (
     <div className={styles.FormItem}>
       <label className={styles.FormItem__label} htmlFor={label}>
@@ -18,6 +19,7 @@ function FormItem({ label, onChange }: IFormItemProps) {
         onChange={(e) => onChange(e.target.value)}
         id={label}
         type={label === 'password' ? 'password' : 'text'}
+        placeholder={placeholder}
       />
     </div>
   );
