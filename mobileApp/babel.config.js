6f85552525bc69eea56cbd8cf4f1b77b8b553a54
Zhengjie Zhou (Jay) <zhengjiezhou@dal.ca>
2023-07-30 11:17:17 -0400

    Fix build

--- mobileApp/babel.config.js
+++ mobileApp/babel.config.js
@@ -1,20 +1,16 @@
-
-
-
 module.exports = function (api) {
   api.cache(true);
   return {
-    
-      presets: ["babel-preset-expo"],
-      plugins: [
-          [
-              "module:react-native-dotenv",
-              {
-                  moduleName: "@env",
-                  path: ".env",
-              },
-          ]
-        
+    presets: ["babel-preset-expo"],
+    plugins: [
+      "expo-router/babel",
+      [
+        "module:react-native-dotenv",
+        {
+          moduleName: "@env",
+          path: ".env",
+        },
       ],
+    ],
   };
 };
--- mobileApp/readme.md
+++ mobileApp/readme.md
@@ -3,12 +3,18 @@
 `npx expo login`  
 `npx expo start`
 
+## Testing
+
+`npx expo start --no-dev --minify` to run in production mode
+
 ## Build & Deployment
 
 Creata app on Apple Store connect  
 Go to `app.json`, change slug, eas project, owner... to your eas project
 Also change id in `app.json` for each deployment build  
 `eas login`  
-`eas build:configure`  
+`eas build:configure`
+then
 `eas build --platform ios`  
 `eas submit -p ios --latest`
+or just `eas build -p ios --auto-submit`
