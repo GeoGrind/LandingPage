80a8d9fdd682394b1d81db0e8e4e0194b255dd40
Delbert <delbert3luo@gmail.com>
2023-08-01 21:54:45 -0400

    more marker

--- desktopApp/src/components/Map/MapPopup/MapPopup.tsx
+++ desktopApp/src/components/Map/MapPopup/MapPopup.tsx
@@ -9,17 +9,29 @@ interface IMapPopupProps {
 function MapPopup({ user }: IMapPopupProps) {
   return (
     <Popup className={styles.MapPopup}>
-      <div className={styles.MapPopup__username}>{user.username}</div>
-      <div className={styles.MapPopup__description}>
-        {user.onGoingSession?.description}
+      <div className={styles.MapPopup__container}>
+        <div className={styles.MapPopup__top}>
+          <div className={styles.MapPopup__top__status}>status</div>
+
+          <div className={styles.MapPopup__top__username}>{user.username}</div>
+          <div className={styles.MapPopup__top__course}>
+            {user.onGoingSession?.course}
+          </div>
+        </div>
+        <div className={styles.MapPopup__description}>
+          desc: {user.onGoingSession?.description}
+        </div>
+
+        <div className={styles.MapPopup__buttons}>
+          <button
+            className={styles.MapPopup__button}
+            type="button"
+            onClick={() => alert('test')}
+          >
+            session info
+          </button>
+        </div>
       </div>
-      <button
-        className={styles.MapPopup__button}
-        type="button"
-        onClick={() => alert('test')}
-      >
-        session info
-      </button>
     </Popup>
   );
 }
