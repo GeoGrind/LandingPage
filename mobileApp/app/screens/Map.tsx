ee255e21e568a8892d727eb98bb008c0b40aa16c
Justin-Jingcheng-Peng <jingchengpeng@yahoo.com>
2023-08-12 18:06:01 -0400

    count down timer clock working

--- mobileApp/app/screens/Map.tsx
+++ mobileApp/app/screens/Map.tsx
@@ -3,11 +3,11 @@ import MapView, { Marker } from "react-native-maps";
 import {
   StyleSheet,
   View,
-  Modal,
   TextInput,
   TouchableOpacity,
   Text,
   AppState,
+  Alert,
 } from "react-native";
 import "firebase/firestore";
 import {
@@ -30,10 +30,15 @@ import { getUserById } from "../utils/db";
 import { updateCurrentUser } from "../store/features/currentUserSlice";
 import { setCurrentUser } from "../store/features/currentUserSlice";
 import { useSelector } from "react-redux";
+
+import { Modal, Portal, PaperProvider } from "react-native-paper";
+import DropDownPicker from "react-native-dropdown-picker";
+import SessionModal from "../components/SessionModal";
+import { CountdownCircleTimer } from "react-native-countdown-circle-timer";
+
 const Map = () => {
   const [inSessionUsers, setInSessionUsers] = useState<User[]>([]);
   const [showForm, setShowForm] = useState(false);
-  const [formValues, setFormValues] = useState({ course: "" });
   const navigation = useNavigation<NativeStackNavigationProp<ParamListBase>>();
   const appState = useRef(AppState.currentState);
   const { currentUser } = FIREBASE_AUTH;
@@ -43,12 +48,20 @@ const Map = () => {
   const currentUserRedux = useSelector(
     (state: any) => state.currentUser.currentUser
   );
+
+  // Modal for starting session
+  const [courseValue, setCourseValue] = useState<string | null>(null);
+  const [timeValue, setTimeValue] = useState<number | null>(null);
+  const [descriptionValue, setDescriptionValue] = useState<string | null>(null);
+  // end of Modal for starting session
+
   // Bottom sheet logic
   const bottomSheetRef = useRef<BottomSheet>(null);
   const handleOpenPress = (user: User) => {
     setClickedUser(user);
     bottomSheetRef.current?.expand();
   };
+
   // End of Bottom sheet logic
 
   // Set up redux when it mounts
@@ -102,21 +115,24 @@ const Map = () => {
     /* TODO: This process is very slow, needs optimization 
     Try to update the UI at client side first, then update the DB in the background
     */
+
+    if (!courseValue || !timeValue || !descriptionValue) {
+      alert("Fill out all fields");
+      return;
+    }
     setShowForm(false);
     const newSession: Session = {
-      course: formValues.course,
+      course: courseValue,
       startTime: Date.now(),
       isVisible: true,
       sessionStartLocation: { longitude: 0, latitude: 0 }, // Set initial location
       numberOfCheerers: 0,
       cheerers: [],
-
-      stopTime: -1,
-      description: "test",
+      stopTime: Date.now() + timeValue,
+      description: descriptionValue,
     };
     const curLocation = await getUserLocation();
     newSession.sessionStartLocation = curLocation;
-
     dispatch(
       updateCurrentUser({
         location: curLocation,
@@ -182,122 +198,141 @@ const Map = () => {
   };
 
   return (
-    <View style={styles.container}>
-      <MapView
-        style={styles.map}
-        onTouchStart={() => {
-          // Dismiss the keyboard when the user taps on the map
-          Keyboard.dismiss();
-        }}
-      >
-        {filterUsers().map((user, index) => {
-          if (user.uid === currentUser?.uid) {
-            return null;
-          }
-          return (
+    <PaperProvider>
+      <View style={styles.container}>
+        <MapView
+          style={styles.map}
+          onTouchStart={() => {
+            // Dismiss the keyboard when the user taps on the map
+            Keyboard.dismiss();
+          }}
+        >
+          {filterUsers().map((user, index) => {
+            if (user.uid === currentUser?.uid) {
+              return null;
+            }
+            return (
+              <Marker
+                key={index}
+                coordinate={{
+                  latitude: user.location!.latitude,
+                  longitude: user.location!.longitude,
+                }}
+                onPress={() => {
+                  handleOpenPress(user);
+                }}
+              >
+                <Text>{user.emoji}</Text>
+              </Marker>
+            );
+          })}
+          {currentUserRedux?.isInSession && (
             <Marker
-              key={index}
+              key={10000000}
               coordinate={{
-                latitude: user.location!.latitude,
-                longitude: user.location!.longitude,
+                latitude: currentUserRedux?.location.latitude,
+                longitude: currentUserRedux?.location.longitude,
               }}
               onPress={() => {
-                handleOpenPress(user);
+                handleOpenPress(currentUserRedux);
               }}
             >
-              <Text>{user.emoji}</Text>
+              <Text>{currentUserRedux?.emoji}</Text>
             </Marker>
-          );
-        })}
-        {currentUserRedux?.isInSession && (
-          <Marker
-            key={10000000}
-            coordinate={{
-              latitude: currentUserRedux?.location.latitude,
-              longitude: currentUserRedux?.location.longitude,
+          )}
+        </MapView>
+        <View style={styles.searchContainer}>
+          {currentUserRedux?.isInSession && (
+            <CountdownCircleTimer
+              isPlaying
+              duration={
+                (currentUserRedux.onGoingSession.stopTime -
+                  currentUserRedux.onGoingSession.startTime) /
+                1000
+              }
+              colors={["#004777", "#F7B801", "#A30000", "#A30000"]}
+              colorsTime={[7, 5, 2, 0]}
+              size={50}
+              strokeWidth={6}
+              onComplete={() => {
+                handleStopSessionClick();
+              }}
+            >
+              {({ remainingTime }) => {
+                const hours = Math.floor(remainingTime / 3600);
+                const minutes = Math.floor((remainingTime % 3600) / 60);
+                return <Text>{`${hours}:${minutes}`}</Text>;
+              }}
+            </CountdownCircleTimer>
+          )}
+          <TextInput
+            style={styles.searchBar}
+            placeholder={"Search"}
+            placeholderTextColor={"#666"}
+            value={input}
+            onChangeText={(s) => {
+              setInput(s);
+              console.log(s);
             }}
-            onPress={() => {
-              handleOpenPress(currentUserRedux);
-            }}
-          >
-            <Text>{currentUserRedux?.emoji}</Text>
-          </Marker>
-        )}
-      </MapView>
-      <View style={styles.searchContainer}>
-        <TextInput
-          style={styles.searchBar}
-          placeholder={"Search"}
-          placeholderTextColor={"#666"}
-          value={input}
-          onChangeText={(s) => {
-            setInput(s);
-            console.log(s);
-          }}
-        />
-        <TouchableOpacity onPress={fetchData}>
-          <FontAwesome5 name="sync" size={30} color="black" />
-        </TouchableOpacity>
-      </View>
-
-      <View style={styles.roundButton}>
-        <FontAwesome5
-          name="play"
-          size={24}
-          color="white"
-          onPress={() => setShowForm(true)}
-        />
-      </View>
-      <View style={styles.buttonContainer}>
-        <Navbar
-          onStopSessionClick={handleStopSessionClick}
-          onSignOffClick={handleSignOffClick}
-          onTestClick={() => {
-            navigation.navigate("Test");
-          }}
-          onListViewClick={() => {
-            navigation.navigate("ListView");
-          }}
-          onChatClick={() => {
-            navigation.navigate("AllChats");
-          }}
-          onSettingClick={handleSettingClick}
-        />
-      </View>
-
-      {showForm && (
-        <Modal visible={showForm} transparent>
-          <TouchableOpacity
-            style={styles.modalContainer}
-            activeOpacity={1}
-            onPress={() => setShowForm(false)}
-          >
-            <View style={styles.formContainer}>
-              <TextInput
-                style={styles.input}
-                placeholder="Course Name"
-                value={formValues.course}
-                onChangeText={(text) => setFormValues({ course: text })}
-              />
-              <TouchableOpacity
-                style={styles.submitButton}
-                onPress={handleFormSubmit}
-              >
-                <Text style={styles.submitButtonText}>Submit</Text>
-              </TouchableOpacity>
-            </View>
+          />
+          <TouchableOpacity onPress={fetchData}>
+            <FontAwesome5 name="sync" size={30} color="black" />
           </TouchableOpacity>
-        </Modal>
-      )}
+        </View>
+        {!currentUserRedux?.isInSession && (
+          <View style={styles.roundButton}>
+            <FontAwesome5
+              name="play"
+              size={24}
+              color="white"
+              onPress={() => setShowForm(true)}
+            />
+          </View>
+        )}
+        <View style={styles.buttonContainer}>
+          <Navbar
+            onStopSessionClick={handleStopSessionClick}
+            onSignOffClick={handleSignOffClick}
+            onTestClick={() => {
+              navigation.navigate("Test");
+            }}
+            onListViewClick={() => {
+              navigation.navigate("ListView");
+            }}
+            onChatClick={() => {
+              navigation.navigate("AllChats");
+            }}
+            onSettingClick={handleSettingClick}
+          />
+        </View>
 
-      {clickedUser && (
-        <CustomizableBottomSheet
-          bottomSheetRef={bottomSheetRef}
-          sheetContent="Awesome 🎉"
-          userMarker={clickedUser}
-        />
-      )}
-    </View>
+        <Portal>
+          <Modal
+            visible={showForm}
+            onDismiss={() => setShowForm(false)}
+            contentContainerStyle={styles.modalContainer}
+          >
+            <SessionModal
+              courseValue={courseValue}
+              timeValue={timeValue}
+              descriptionValue={descriptionValue}
+              setCourseValue={setCourseValue}
+              setTimeValue={setTimeValue}
+              setDescriptionValue={setDescriptionValue}
+              handleFormSubmit={handleFormSubmit}
+            />
+          </Modal>
+        </Portal>
+
+        {clickedUser && (
+          <CustomizableBottomSheet
+            bottomSheetRef={bottomSheetRef}
+            sheetContent="Awesome 🎉"
+            userMarker={clickedUser}
+          />
+        )}
+      </View>
+    </PaperProvider>
   );
 };
 
@@ -358,10 +393,12 @@ const styles = StyleSheet.create({
     textAlign: "center",
   },
   modalContainer: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-    backgroundColor: "rgba(0, 0, 0, 0.5)",
+    backgroundColor: "white",
+    padding: 20,
+    width: "80%", // 80% of the screen width
+    height: "50%", // 50% of the screen height
+    alignSelf: "center",
+    borderRadius: 5,
   },
   roundButton: {
     width: 90,
