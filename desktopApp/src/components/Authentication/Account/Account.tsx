abf8a13fc9858933e168e386e88551372f821ec0
Delbert <delbert3luo@gmail.com>
2023-08-13 20:50:58 -0400

    make account work with backend

--- desktopApp/src/components/Authentication/Account/Account.tsx
+++ desktopApp/src/components/Authentication/Account/Account.tsx
@@ -1,36 +1,26 @@
 /* eslint-disable react/jsx-props-no-spreading */
-import React, { useEffect, useState } from 'react';
-import { Link, NavLink, useNavigate } from 'react-router-dom';
-import { signOut } from 'firebase/auth';
+import { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import { useAuthContext } from 'context/AuthContext';
 import { useAppContext } from 'context/AppContext';
 import { useForm } from 'react-hook-form';
 
 import styles from './Account.module.scss';
-import FormItem from '../Form/FormItem/FormItem';
 
 function Account() {
+  const { currentUser, logout, updateCurrentUser } = useAuthContext();
   const { contentStyles } = useAppContext();
-  const { currentUser, logout } = useAuthContext();
-  const navigate = useNavigate();
-  const [username, setUsername] = useState(currentUser?.username);
-  const [yearOfGraduation, setYearOfGraduation] = useState(
-    currentUser?.yearOfGraduation
-  );
-  const [university, setUniversity] = useState(currentUser?.university);
-  const [program, setProgram] = useState(currentUser?.program);
-  const [termCourses, setTermCourses] = useState(currentUser?.termCourses);
-  const [bio, setBio] = useState(currentUser?.bio);
 
   const {
     register,
     handleSubmit,
     formState: { errors },
   } = useForm();
+
   const onSubmit = (data: any) => {
+    updateCurrentUser(data);
     console.log(data);
   };
-  console.log(errors);
 
   if (!currentUser) {
     return null;
@@ -42,44 +32,48 @@ function Account() {
         <input
           className={styles.Account__form__input}
           type="text"
-          value={currentUser.username}
+          defaultValue={currentUser.username}
           placeholder="Username"
-          {...register}
+          {...register('username')}
         />
         <label>Year of Graduation</label>
 
         <input
           className={styles.Account__form__input}
           type="number"
-          value={currentUser.yearOfGraduation}
+          defaultValue={currentUser.yearOfGraduation}
           placeholder="Year of Graduation"
-          {...register('Year of Graduation', { max: 3000, min: 2000 })}
+          {...register('yearOfGraduation', { max: 3000, min: 2000 })}
         />
 
         <label>University</label>
-        <select {...register('University')}>
+        <select
+          defaultValue={currentUser.university || ''}
+          {...register('university')}
+        >
           <option value="University of Waterloo">University of Waterloo</option>
         </select>
         <label>Program</label>
 
-        <select {...register('Program')}>
+        <select defaultValue={currentUser.program} {...register('program')}>
           <option value="Mathematics">Mathematics</option>
         </select>
         <label>Term Courses</label>
         <input
           className={styles.Account__form__input}
+          defaultValue={currentUser.termCourses}
           type="text"
-          placeholder="Term Courses"
-          {...register}
+          placeholder=""
+          {...register('termCourses')}
         />
 
         <label>Bio</label>
         <input
           className={styles.Account__form__input}
-          value={currentUser.bio}
+          defaultValue={currentUser.bio}
           type="text"
-          placeholder="Bio"
-          {...register}
+          placeholder="bio"
+          {...register('bio')}
         />
 
         <div className={styles.Account__form__save}>
