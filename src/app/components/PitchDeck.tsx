0212d4ade716680f4769314574ac3c73724ab63e
Delbert <delbert3luo@gmail.com>
2023-08-29 16:14:52 -0400

    fix build issues

--- src/app/components/PitchDeck.tsx
+++ src/app/components/PitchDeck.tsx
@@ -1,14 +1,13 @@
 const PitchDeck = () => (
-    <section className="px-6 py-20 bg-gradient-to-r from-b0d9ac to-c5dc62">
-      <h2 className="text-4xl font-semibold mb-6">"Pitch Deck"</h2>
-      <ul>
-        {/* Example list of features or selling points */}
-        <li>Geo-located study sessions - join or create one near you!</li>
-        <li>Filter sessions by course - focus on what you're studying.</li>
-        <li>Real-time notifications - never miss a study group again.</li>
-        // Add more items as needed
-      </ul>
-    </section>
-)
+  <section className="px-6 py-20 bg-gradient-to-r from-b0d9ac to-c5dc62">
+    <h2 className="text-4xl font-semibold mb-6">Pitch Deck</h2>
+    <ul>
+      {/* Example list of features or selling points */}
+      <li>Geo-located study sessions - join or create one near you!</li>
+      <li>Filter sessions by course - focus on what you&apos;re studying.</li>
+      <li>Real-time notifications - never miss a study group again.</li>
+    </ul>
+  </section>
+);
 
-export { PitchDeck }
+export { PitchDeck };
