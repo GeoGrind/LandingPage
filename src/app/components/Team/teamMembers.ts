4664580517dfe5b584c90bc22e77072a5fdd3b72
Delbert <delbert3luo@gmail.com>
2023-09-06 18:57:55 -0400

    wip

--- src/app/components/Team/teamMembers.ts
+++ src/app/components/Team/teamMembers.ts
@@ -1,87 +1,97 @@
-import picture from "../../assets/GeoGrindDark.png";
+import JustinPeng from "../../assets/JustinPeng.png";
+import JayZhou from "../../assets/JayZhou.png";
+import DelbertLuo from "../../assets/DelbertLuo.png";
+import ChristopherSimanjuntak from "../../assets/ChristopherSimanjuntak.png";
+import MichaelLapshin from "../../assets/MichaelLapshin.png";
+import KevinShi from "../../assets/KevinShi.png";
+import GloriaKuang from "../../assets/GloriaKuang.png";
+import MilesZaman from "../../assets/MilesZaman.png";
+import EdwardZhang from "../../assets/EdwardZhang.png";
+import KentTran from "../../assets/KentTran.png";
 const teamMembers = [
   {
-    image: picture,
+    image: JustinPeng,
     name: "Justin Peng",
-    headline: "headline",
-    links: { "Linkedin": "https://www.linkedin.com/in/justinpeng1209/" },
+    headline: "Founder/Lead SWE for Mobile — Computer Science 2025 @ Uwaterloo",
+    links: { Linkedin: "https://www.linkedin.com/in/justinpeng1209/" },
     description: "I love coding",
   },
   {
-    image: picture,
+    image: JayZhou,
     name: "Jay Zhou",
-    headline: "headline",
+    headline: "Founder/Devops — Computer Science 2024 @ UWaterloo",
     links: {
-      "Linkedin": "https://www.linkedin.com/in/jay-zhengjie-zhou/",
-      "Github": "https://github.com/JayZhouzzj",
+      Linkedin: "https://www.linkedin.com/in/jay-zhengjie-zhou/",
+      Github: "https://github.com/JayZhouzzj",
     },
     description: "I like cilantro",
   },
   {
-    image: picture,
+    image: DelbertLuo,
     name: "Delbert Luo",
-    headline: "headline",
-    links: { "Linkedin": "https://www.linkedin.com/in/delbertluo/" },
+    headline: "Founder/Lead SWE for Desktop — Mathematics 2026 @ UWaterloo",
+    links: { Linkedin: "https://www.linkedin.com/in/delbertluo/" },
     description: "coder",
   },
   {
-    image: picture,
-    name: "Christopher Simanjuntak",
-    headline: "headline",
-    links: { "Linkedin": "https://www.linkedin.com/in/mschristophers/" },
-    description:
-      "I sometimes create ChatGPT prompts about making movie scripts for me to imagine in my mind",
-  },
-  {
-    image: picture,
-    name: "Michael Lapshin",
-    headline: "headline",
-    links: { "Linkedin": "https://www.linkedin.com/in/michael-lapshin/" },
-    description:
-      "Facts:  I do rock-climbing for fun. I'm experienced in backend & microservices.",
-  },
-  {
-    image: picture,
-    name: "Kevin Shi",
-    headline: "headline",
-    links: { "Linkedin": "https://www.linkedin.com/in/kevin-shi-12a7271b6/" },
-    description: "",
-  },
-  {
-    image: picture,
+    image: GloriaKuang,
     name: "Gloria Kuang",
-    headline: "headline",
+    headline: "Lead Product Designer — New Media 2025 @ University of Toronto",
     links: {
-      "Linkedin": "https://www.linkedin.com/in/gloria-kuang-53427a238/",
+      Linkedin: "https://www.linkedin.com/in/gloria-kuang-53427a238/",
     },
     description:
       " A highly artistic person enjoys a wide array of art, including music, theatre, painting, and visual arts, and also loves staying with kids.",
   },
   {
-    image: picture,
+    image: ChristopherSimanjuntak,
+    name: "Christopher Simanjuntak",
+    headline: "Software Engineer — Mathematics 2026 @ UWaterloo",
+    links: { Linkedin: "https://www.linkedin.com/in/mschristophers/" },
+    description:
+      "I sometimes create ChatGPT prompts about making movie scripts for me to imagine in my mind",
+  },
+  {
+    image: KevinShi,
+    name: "Kevin Shi",
+    headline:
+      "Lead Data Scientist/Backend Engineer — Mathematical Finance + Combinatorics & Optimization 2024 @ UWaterloo",
+    links: { Linkedin: "https://www.linkedin.com/in/kevin-shi-12a7271b6/" },
+    description: "",
+  },
+  {
+    image: EdwardZhang,
     name: "Edward Zhang",
-    headline: "headline",
-    links: { "Linkedin": "https://www.linkedin.com/in/zhang-edward/" },
+    headline: "Software Engineer - Statistics + Computational Mathematics 2026 @ UWaterloo",
+    links: { Linkedin: "https://www.linkedin.com/in/zhang-edward/" },
     description:
       "Fun facts: likes working out, favourite basketball player is harden, majoring in statistics",
   },
   {
-    image: picture,
+    image: KentTran,
+    name: "Kent Tran",
+    headline: "Software Engineer - Mechatronics 2027 @ UWaterloo",
+    links: { Linkedin: "https://www.linkedin.com/in/kent-tran2302" },
+    description: "I like playing soccer for fun",
+  },
+  {
+    image: MichaelLapshin,
+    name: "Michael Lapshin",
+    headline: "Dev - Software Engineering 2025 @ UWaterloo",
+    links: { Linkedin: "https://www.linkedin.com/in/michael-lapshin/" },
+    description:
+      "Facts:  I do rock-climbing for fun. I'm experienced in backend & microservices.",
+  },
+  {
+    image: MilesZaman,
     name: "Miles Zaman",
-    headline: "headline",
+    headline: "Dev - Computer Science 2026 @ Uwaterloo",
     links: {
-      "Linkedin": "https://www.linkedin.com/in/miles-zaman/",
-      "Website": "https://www.mileszaman.com/",
+      Linkedin: "https://www.linkedin.com/in/miles-zaman/",
+      Website: "https://www.mileszaman.com/",
     },
     description:
       "fun fact: I'm into Muay Thai, rock climbing, and I love sushi",
-  },
-  {
-    image: picture,
-    name: "Kent Tran",
-    headline: "headline",
-    links: { "Linkedin": "https://www.linkedin.com/in/kent-tran2302" },
-    description: "I like playing soccer for fun",
   },
 ];
 
