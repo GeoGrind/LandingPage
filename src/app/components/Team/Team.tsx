4f87ffe9127cb22c7f96f1403df122aaf0c938cc
Delbert <delbert3luo@gmail.com>
2023-09-06 18:15:02 -0400

    add links

--- src/app/components/Team/Team.tsx
+++ src/app/components/Team/Team.tsx
@@ -1,30 +1,52 @@
 import Image from "next/image";
 import teamMembers from "./teamMembers";
 import styles from "./Team.module.scss";
-const Team = () => (
-  // <section className="mx-6 py-20">
-  <section className={styles.Team}>
-    <h2 className="text-4xl font-semibold mb-6 text-center">Meet the Team</h2>
-    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-1 gap-8">
-      {teamMembers.map((member) => (
-        // <div key={member.name} className="flex flex-row items-center space-y-2">
-        <div key={member.name} className="flex flex-row items-center space-y-2">
-          <Image
-            src={member.img}
-            alt={member.name}
-            className="rounded-full"
-            width={200}
-            height={200}
-          />
-          <div className="text-left">
-            <h3 className="text-2xl font-semibold mb-1">{member.name}</h3>
-            <p>{member.headline}</p>
-            <p>{member.description}</p>
+const Team = () => {
+  return (
+    // <section className="mx-6 py-20">
+    <section className={styles.Team}>
+      <h2 className="text-4xl font-semibold mb-6 text-center">Meet the Team</h2>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-1 gap-8">
+        {teamMembers.map((member) => (
+          // <div key={member.name} className="flex flex-row items-center space-y-2">
+          // <div key={member.name} className="flex flex-row items-center space-y-2">
+          <div key={member.name} className={styles.Team__member}>
+            <Image
+              src={member.image}
+              alt={member.name}
+              className={styles.Team__member__image}
+            />
+            <div className={styles.Team__member__content}>
+              <div className={styles.Team__member__content__name}>
+                {member.name}
+              </div>
+              <div className={styles.Team__member__content__headline}>
+                {member.headline}
+              </div>
+              <div className={styles.Team__member__content__links}>
+                {Object.entries(member.links).map((link) => {
+                  return (
+                    <a
+                      key={link[0]}
+                      href={link[1]}
+                      target="_blank"
+                      rel="noopener no referrer"
+                      className={styles.Team__member__content__links__link}
+                    >
+                      {link[0]}
+                    </a>
+                  );
+                })}
+              </div>
+              <div className={styles.Team__member__content__description}>
+                {member.description}
+              </div>
+            </div>
           </div>
-        </div>
-      ))}
-    </div>
-  </section>
-);
+        ))}
+      </div>
+    </section>
+  );
+};
 
 export { Team };
--- src/app/components/Team/teamMembers.ts
+++ src/app/components/Team/teamMembers.ts
@@ -1,69 +1,86 @@
 import picture from "../../assets/GeoGrindDark.png";
-
 const teamMembers = [
   {
+    image: picture,
     name: "Justin Peng",
-    img: picture,
     headline: "headline",
+    links: { "Linkedin": "https://www.linkedin.com/in/justinpeng1209/" },
     description: "I love coding",
   },
   {
+    image: picture,
     name: "Jay Zhou",
-    img: picture,
     headline: "headline",
+    links: {
+      "Linkedin": "https://www.linkedin.com/in/jay-zhengjie-zhou/",
+      "Github": "https://github.com/JayZhouzzj",
+    },
     description: "I like cilantro",
   },
   {
+    image: picture,
     name: "Delbert Luo",
-    img: picture,
     headline: "headline",
+    links: { "Linkedin": "https://www.linkedin.com/in/delbertluo/" },
     description: "coder",
   },
   {
+    image: picture,
     name: "Christopher Simanjuntak",
-    img: picture,
     headline: "headline",
+    links: { "Linkedin": "https://www.linkedin.com/in/mschristophers/" },
     description:
       "I sometimes create ChatGPT prompts about making movie scripts for me to imagine in my mind",
   },
   {
+    image: picture,
     name: "Michael Lapshin",
-    img: picture,
     headline: "headline",
+    links: { "Linkedin": "https://www.linkedin.com/in/michael-lapshin/" },
     description:
       "Facts:  I do rock-climbing for fun. I'm experienced in backend & microservices.",
   },
   {
+    image: picture,
     name: "Kevin Shi",
-    img: picture,
     headline: "headline",
+    links: { "Linkedin": "https://www.linkedin.com/in/kevin-shi-12a7271b6/" },
     description: "",
   },
   {
+    image: picture,
     name: "Gloria Kuang",
-    img: picture,
     headline: "headline",
+    links: {
+      "Linkedin": "https://www.linkedin.com/in/gloria-kuang-53427a238/",
+    },
     description:
       " A highly artistic person enjoys a wide array of art, including music, theatre, painting, and visual arts, and also loves staying with kids.",
   },
   {
+    image: picture,
     name: "Edward Zhang",
-    img: picture,
     headline: "headline",
+    links: { "Linkedin": "https://www.linkedin.com/in/zhang-edward/" },
     description:
       "Fun facts: likes working out, favourite basketball player is harden, majoring in statistics",
   },
   {
+    image: picture,
     name: "Miles Zaman",
-    img: picture,
     headline: "headline",
+    links: {
+      "Linkedin": "https://www.linkedin.com/in/miles-zaman/",
+      "Website": "https://www.mileszaman.com/",
+    },
     description:
       "fun fact: I'm into Muay Thai, rock climbing, and I love sushi",
   },
   {
+    image: picture,
     name: "Kent Tran",
-    img: picture,
     headline: "headline",
+    links: { "Linkedin": "https://www.linkedin.com/in/kent-tran2302" },
     description: "I like playing soccer for fun",
   },
 ];
