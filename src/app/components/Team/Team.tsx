121848a11b7808087f8052206440ce4267e3ff35
Delbert <delbert3luo@gmail.com>
2023-09-06 21:24:52 -0400

    fix some styles

--- src/app/components/Team/Team.tsx
+++ src/app/components/Team/Team.tsx
@@ -3,13 +3,10 @@ import teamMembers from "./teamMembers";
 import styles from "./Team.module.scss";
 const Team = () => {
   return (
-    // <section className="mx-6 py-20">
     <section className={styles.Team}>
       <h2 className="text-4xl font-semibold mb-6 text-center">Meet the Team</h2>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-1 gap-8">
         {teamMembers.map((member) => (
-          // <div key={member.name} className="flex flex-row items-center space-y-2">
-          // <div key={member.name} className="flex flex-row items-center space-y-2">
           <div key={member.name} className={styles.Team__member}>
             <Image
               src={member.image}
