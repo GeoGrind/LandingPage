ca97137b263e5489b4136f8eb256004d63ac4c4f
Delbert <delbert3luo@gmail.com>
2023-09-06 21:24:40 -0400

    team member data done

--- src/app/components/Team/teamMembers.ts
+++ src/app/components/Team/teamMembers.ts
@@ -12,86 +12,94 @@ const teamMembers = [
   {
     image: JustinPeng,
     name: "Justin Peng",
-    headline: "Founder/Lead SWE for Mobile — Computer Science 2025 @ Uwaterloo",
+    headline: "Founder/SWE Lead for Mobile — Computer Science 2026 @ Uwaterloo",
     links: { Linkedin: "https://www.linkedin.com/in/justinpeng1209/" },
-    description: "I love coding",
+    description: "Justin loves coding.",
   },
   {
     image: JayZhou,
     name: "Jay Zhou",
-    headline: "Founder/Devops — Computer Science 2024 @ UWaterloo",
+    headline:
+      "Founder/Platform & Production Engineer — Computer Science 2024 @ UWaterloo",
     links: {
       Linkedin: "https://www.linkedin.com/in/jay-zhengjie-zhou/",
       Github: "https://github.com/JayZhouzzj",
     },
-    description: "I like cilantro",
+    description: "Jay likes cilantro.",
   },
   {
     image: DelbertLuo,
     name: "Delbert Luo",
-    headline: "Founder/Lead SWE for Desktop — Mathematics 2026 @ UWaterloo",
-    links: { Linkedin: "https://www.linkedin.com/in/delbertluo/" },
-    description: "coder",
+    headline:
+      "Founder/SWE Lead for Desktop —  Combinatorics & Optimization 2026 @ UWaterloo",
+    links: {
+      Linkedin: "https://www.linkedin.com/in/delbertluo/",
+      Website: "https://delberter.com/",
+    },
+    description:
+      "Delbert's favourite colour is purple. When he's not rock climbing he enjoys playing badminton, soccer, and dodgeball.",
   },
   {
     image: GloriaKuang,
     name: "Gloria Kuang",
-    headline: "Lead Product Designer — New Media 2025 @ University of Toronto",
+    headline: "Product Designer — New Media 2025 @ University of Toronto",
     links: {
       Linkedin: "https://www.linkedin.com/in/gloria-kuang-53427a238/",
     },
     description:
-      " A highly artistic person enjoys a wide array of art, including music, theatre, painting, and visual arts, and also loves staying with kids.",
+      "Gloria is a highly artistic person who enjoys a wide array of art, including music, theatre, painting, and visual arts. She also loves staying with kids.",
   },
   {
     image: ChristopherSimanjuntak,
     name: "Christopher Simanjuntak",
-    headline: "Software Engineer — Mathematics 2026 @ UWaterloo",
+    headline:
+      "Business Analyst/Software Engineer — Mathematics 2026 @ UWaterloo",
     links: { Linkedin: "https://www.linkedin.com/in/mschristophers/" },
     description:
-      "I sometimes create ChatGPT prompts about making movie scripts for me to imagine in my mind",
+      "Christopher sometimes creates ChatGPT prompts about making movie scripts for him to imagine in his mind.",
   },
   {
     image: KevinShi,
     name: "Kevin Shi",
     headline:
-      "Lead Data Scientist/Backend Engineer — Mathematical Finance + Combinatorics & Optimization 2024 @ UWaterloo",
+      "Data Scientist/Backend Engineer — Mathematical Finance + Combinatorics & Optimization 2024 @ UWaterloo",
     links: { Linkedin: "https://www.linkedin.com/in/kevin-shi-12a7271b6/" },
-    description: "",
+    description:
+      "Kevin is a math student who is interested in the application of computational methods and statistical modeling in the real world.",
   },
   {
     image: EdwardZhang,
     name: "Edward Zhang",
-    headline: "Software Engineer - Statistics + Computational Mathematics 2026 @ UWaterloo",
+    headline:
+      "Software Engineer - Statistics + Computational Mathematics 2026 @ UWaterloo",
     links: { Linkedin: "https://www.linkedin.com/in/zhang-edward/" },
     description:
-      "Fun facts: likes working out, favourite basketball player is harden, majoring in statistics",
+      "Edward is majoring in Statistics and likes working out, his favourite basketball player is James Harden.",
   },
   {
     image: KentTran,
     name: "Kent Tran",
-    headline: "Software Engineer - Mechatronics 2027 @ UWaterloo",
+    headline: "Backend Engineer - Mechatronics 2027 @ UWaterloo",
     links: { Linkedin: "https://www.linkedin.com/in/kent-tran2302" },
-    description: "I like playing soccer for fun",
+    description: "Kent likes playing soccer for fun.",
   },
   {
     image: MichaelLapshin,
     name: "Michael Lapshin",
-    headline: "Dev - Software Engineering 2025 @ UWaterloo",
+    headline: "Business Analyst - Software Engineering 2025 @ UWaterloo",
     links: { Linkedin: "https://www.linkedin.com/in/michael-lapshin/" },
     description:
-      "Facts:  I do rock-climbing for fun. I'm experienced in backend & microservices.",
+      "Michael does rock-climbing for fun. He's also experienced in backend & microservices.",
   },
   {
     image: MilesZaman,
     name: "Miles Zaman",
-    headline: "Dev - Computer Science 2026 @ Uwaterloo",
+    headline: "Software Engineer - Computer Science 2026 @ Uwaterloo",
     links: {
       Linkedin: "https://www.linkedin.com/in/miles-zaman/",
       Website: "https://www.mileszaman.com/",
     },
-    description:
-      "fun fact: I'm into Muay Thai, rock climbing, and I love sushi",
+    description: "Miles is into Muay Thai, rock climbing, and loves sushi.",
   },
 ];
 
